e938f59d34670869c9fd5af1a93fe1dde40e82f4
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-08-14 21:19:42 -0400

    Create the table vets and the tables specializations and visits with the relation many to many

--- schema.sql
+++ schema.sql
@@ -37,3 +37,30 @@ ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
 
 ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
+
+
+/* New tables in relations many-to-many */
+
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    age INT,
+    date_of_graduation DATE
+);
+
+-- Join table between species and vets
+CREATE TABLE specializations (
+    vet_id INT REFERENCES vets(id),
+    species_id INT REFERENCES species(id),
+    PRIMARY KEY (vet_id, species_id)
+);
+
+-- Join table between animals and vets
+CREATE TABLE visits (
+    animal_id INT REFERENCES animals(id),
+    vet_id INT REFERENCES vets(id),
+    visit_date DATE,
+    PRIMARY KEY (animal_id, vet_id)
+);
+
+ALTER TABLE visits DROP CONSTRAINT visits_pkey;
