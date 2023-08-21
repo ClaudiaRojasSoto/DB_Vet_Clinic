32964a335dd0b75c8f8932d3990696b34d8cf722
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-08-21 12:27:30 -0400

    Delete the analyze queries from the schema and add the queries on the quries.sql file

--- queries.sql
+++ queries.sql
@@ -233,3 +233,8 @@ GROUP BY s.name
 ORDER BY COUNT(*) DESC
 LIMIT 1;
 
+explain analyze SELECT COUNT(*) FROM visits where animal_id = 4;
+
+explain analyze SELECT * FROM visits where vet_id = 2;
+
+explain analyze SELECT * FROM owners where email = 'owner_18327@mail.com';
